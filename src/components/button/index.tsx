5d514bd5d4a2549731088db7465986977e3a3258
Adao Junior <adao8678@gmail.com>
2022-11-21 23:42:03 -0300

    Created form component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TesteButton extends React.Component {
+    render(){
+        return(
+            <button>
+                Button
+            </button>
+        )
+    }
+}
+
+export default TesteButton
